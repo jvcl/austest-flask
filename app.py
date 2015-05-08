989ead851f2412172d38cfc4dbafa5f4e526ea23
jvcl <valdi.jorge@gmail.com>
2015-05-08 17:48:36 +0930

    fix template for results

--- app.py
+++ app.py
@@ -72,9 +72,10 @@ def test():
         dic[question] = question.answers.all()
     return render_template('test.html', questions=dic)
 
+
 @app.route('/about')
 def about():
-    render_template('about.html')
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
